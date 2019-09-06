f2b8348119d607313ac4cfee60d2a4e2073a1454
阿鲲 <@DESKTOP-QIPKS64.lan>
2019-09-06 08:43:17 +0800

    后端完整程序

---
+++ APPInfo/src/cn/appsys/dao/appversion/AppversionMapper.java
@@ -0,0 +1,23 @@
+package cn.appsys.dao.appversion;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
+import cn.appsys.pojo.AppVersion;
+
+public interface AppversionMapper {
+public List<AppVersion> getAppVersionList(@Param("id")int id);
+	
+	public int insAppVersion(AppVersion appVersion);
+	
+	public AppVersion getAppVersion(@Param("id")int id);
+	
+	public int delapk(@Param("id")int id);
+	
+	public int updAppVersion(AppVersion appVersion);
+	
+	public AppVersion getAppVersionxing();
+	
+	public int delAppVersion(@Param("appId")int appId);
+}
